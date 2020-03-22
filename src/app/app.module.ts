8fecfdc11c2fdda6d243a3b5057251c6c9d3e983
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-22 13:19:16 +0530

    Created admin-header component Added a placeholder navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
 
 import { Routes, RouterModule } from '@angular/router';
+import { AdminHeaderComponent } from './admin/admin-header/admin-header.component';
 
 const routes: Routes = [{ path: 'admin', component: AdminComponent }];
 
 @NgModule({
-  declarations: [AppComponent, AdminComponent],
+  declarations: [AppComponent, AdminComponent, AdminHeaderComponent],
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
