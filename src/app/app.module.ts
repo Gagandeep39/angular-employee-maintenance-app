a5fa5bbc41bcad41128704d1ab5a455952ce4398
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-22 13:16:24 +0530

    Adding Routing for admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
 
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [{ path: 'admin', component: AdminComponent }];
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    AdminComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, AdminComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
