03e9b5be739942bfdd81d80cd76d445295cd1744
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-22 13:37:25 +0530

    Created add, list employee components Added nested routes to the components Created router outlet for nested component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,29 @@ import { AdminComponent } from './admin/admin.component';
 
 import { Routes, RouterModule } from '@angular/router';
 import { AdminHeaderComponent } from './admin/admin-header/admin-header.component';
+import { EmployeeAddComponent } from './admin/employee-add/employee-add.component';
+import { EmployeeListComponent } from './admin/employee-list/employee-list.component';
 
-const routes: Routes = [{ path: 'admin', component: AdminComponent }];
+const routes: Routes = [
+  {
+    path: 'admin',
+    component: AdminComponent,
+    children: [
+      { path: 'list', component: EmployeeListComponent },
+      { path: 'add', component: EmployeeAddComponent },
+      { path: ':id/update', component: EmployeeAddComponent }
+    ]
+  }
+];
 
 @NgModule({
-  declarations: [AppComponent, AdminComponent, AdminHeaderComponent],
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    AdminHeaderComponent,
+    EmployeeAddComponent,
+    EmployeeListComponent
+  ],
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
