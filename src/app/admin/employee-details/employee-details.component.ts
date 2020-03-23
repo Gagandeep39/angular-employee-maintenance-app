af1019c70e318f25a3e41b6229f69bb47a991b51
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-24 01:11:51 +0530

    Added code to fetch employee details Added a dummy details UI Added Css classes to improve UI

--- src/app/admin/employee-details/employee-details.component.ts
+++ src/app/admin/employee-details/employee-details.component.ts
@@ -1,4 +1,7 @@
+import { Employee } from './../../models/employee.model';
+import { AdminService } from 'src/app/services/admin.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-employee-details',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-details.component.css']
 })
 export class EmployeeDetailsComponent implements OnInit {
+  employee: Employee;
+  empId: number;
 
-  constructor() { }
+  constructor(private service: AdminService, private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.params.subscribe((param: Params) => {
+      this.empId = param['id'];
+      this.employee = this.service.fetchEmployeeById(this.empId);
+    });
   }
-
 }
