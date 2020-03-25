914eae4bff08947eaaedead07c1e5be3583ef232
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-25 17:55:15 +0530

    Added address model Employee address now consits of multiple fields Employee List now Shows city Disabled edit button in emp details for now Address in emp detail dont show object anymore

---
+++ src/app/models/address.model.ts
@@ -0,0 +1,14 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-03-25 17:41:33
+ * @modify date 2020-03-25 17:41:33
+ * @desc Stores Address of the user
+ */
+
+export class Address {
+  street: string;
+  landmark: string;
+  city: string;
+  pincode: string;
+}
