8df80705e2f1d560963f2d71bd22598fce62ce36
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-25 12:04:25 +0530

    Created user model Created usertype enum

---
+++ src/app/models/user-type.model.ts
@@ -0,0 +1,12 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-03-25 12:04:04
+ * @modify date 2020-03-25 12:04:04
+ * @desc UserType contains list of possible usertypes
+ */
+
+export enum UserType {
+  Admin = 'Admin',
+  Employee = 'Employee'
+}
